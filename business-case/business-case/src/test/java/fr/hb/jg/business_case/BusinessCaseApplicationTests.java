7b956ef558d79b5e9a0a6650402929c313edd12e
Alkalote <witherkiller1109@gmail.com>
2024-10-08 11:10:30 +0200

    initial commit

---
+++ business-case/business-case/src/test/java/fr/hb/jg/business_case/BusinessCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.hb.jg.business_case;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
